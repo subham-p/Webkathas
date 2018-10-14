37ff9a20f96503c8a76f41566fcb4081da6824f8
Web Kathas <webkathas@gmail.com>
2018-10-14 17:38:21 +0000

    add 404 html and css

--- app.js
+++ app.js
@@ -86,6 +86,33 @@ app.use(commentRoutes);
 
 
 
+// Since this is the last non-error-handling
+// middleware use()d, we assume 404, as nothing else
+// responded.
+
+// $ curl http://localhost:3000/notfound
+// $ curl http://localhost:3000/notfound -H "Accept: application/json"
+// $ curl http://localhost:3000/notfound -H "Accept: text/plain"
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+//   // respond with json
+//   if (req.accepts('json')) {
+//     res.send({ error: 'Not found' });
+//     return;
+//   }
+
+//   // default to plain-text. send()
+//   res.type('txt').send('Not found');
+});
+
 
 
 
