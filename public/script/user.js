06c2131af5aed15cd68bd2b31334e0e4a17e1cb3
Web Kathas <webkathas@gmail.com>
2018-10-17 11:56:20 +0000

    change user settings, change carousel width according the screen

--- public/script/user.js
+++ public/script/user.js
@@ -53,48 +53,51 @@ $('#profSettings').hover(function(){
         
         
         
-        $('#library').click(function(){
+        $('#library .list-group-item').css({"background":"wheat","color":"black"});
+         $('#library').click(function(){
             $('.comment').hide();
              $('.reviews').hide();
        $('.library').show();
         $('.profSettings').hide();
-       $('#library .list-group-item').addClass("hvr-radial-out");
-       $('#comment .list-group-item').removeClass("hvr-radial-out");
-       $('#profSettings .list-group-item').removeClass("hvr-radial-out");
-       $('#reviews .list-group-item').removeClass("hvr-radial-out");
+         $('#library .list-group-item').css({"background":"wheat","color":"black"});
+       $('#comment .list-group-item').css({"background":"black","color":"wheat"});
+       $('#profSettings .list-group-item').css({"background":"black","color":"wheat"});
+       $('#reviews .list-group-item').css({"background":"black","color":"wheat"});
 });
+
 $('#comment').click(function(){
           $('.comment').show();
              $('.reviews').hide();
        $('.library').hide();
         $('.profSettings').hide();
-        $('#library .list-group-item').removeClass("hvr-radial-out");
-       $('#comment .list-group-item').addClass("hvr-radial-out");
-       $('#profSettings .list-group-item').removeClass("hvr-radial-out");
-       $('#reviews .list-group-item').removeClass("hvr-radial-out");
-});    
+         $('#library .list-group-item').css({"background":"black","color":"wheat"});
+       $('#comment .list-group-item').css({"background":"wheat","color":"black"});
+       $('#profSettings .list-group-item').css({"background":"black","color":"wheat"});
+       $('#reviews .list-group-item').css({"background":"black","color":"wheat"});
+
+});
 
 $('#reviews').click(function(){
           $('.reviews').show();
              $('.comment').hide();
        $('.library').hide();
         $('.profSettings').hide();
-        $('#library .list-group-item').removeClass("hvr-radial-out");
-       $('#comment .list-group-item').removeClass("hvr-radial-out");
-       $('#profSettings .list-group-item').removeClass("hvr-radial-out");
-       $('#reviews .list-group-item').addClass("hvr-radial-out");
+        $('#library .list-group-item').css({"background":"black","color":"wheat"});
+       $('#comment .list-group-item').css({"background":"black","color":"wheat"});
+       $('#profSettings .list-group-item').css({"background":"black","color":"wheat"});
+       $('#reviews .list-group-item').css({"background":"wheat","color":"black"});
 });  
-
 
 $('#profSettings').click(function(){
           $('.profSettings').show();
            $('.reviews').hide();
              $('.comment').hide();
        $('.library').hide();
-        $('#library .list-group-item').removeClass("hvr-radial-out");
-       $('#comment .list-group-item').removeClass("hvr-radial-out");
-       $('#reviews .list-group-item').removeClass("hvr-radial-out");
-       $('#profSettings .list-group-item').addClass("hvr-radial-out");
-});  
+         $('#library .list-group-item').css({"background":"black","color":"wheat"});
+       $('#comment .list-group-item').css({"background":"black","color":"wheat"});
+       $('#profSettings .list-group-item').css({"background":"wheat","color":"black"});
+       $('#reviews .list-group-item').css({"background":"black","color":"wheat"});
+  
+    }); 
     }); 
 });
