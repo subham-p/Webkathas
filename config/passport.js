0e709ad221888fa01697162f8d40bbd20be08051
Web Kathas <webkathas@gmail.com>
2018-10-19 06:50:12 +0000

    google oauth check

--- config/passport.js
+++ config/passport.js
@@ -86,68 +86,68 @@ module.exports = function(passport) {
     
     
     
- //   passport.use(new FacebookStrategy({
-	//     clientID: process.env.FACEBOOK_CLIENT_ID,
-	//     clientSecret: process.env.FACEBOOK_SECRET,
-	//     callbackURL: process.env.FACEBOOK_URL,
-	//     profileFields: ['email','first_name','last_name','gender','profileUrl','picture.type(large)']
-	//   },
-	//   function(accessToken, refreshToken, profile, done) {
-	//     	process.nextTick(function(){
-	//     		User.findOne({'facebook.id': profile.id}, function(err, user){
-	//     			if(err)
-	//     				return done(err);
-	//     			if(user)
-	//     			{
-	//     					return done(null, user);
-	//     			}
-	//     			else {
+    passport.use(new FacebookStrategy({
+	    clientID: process.env.FACEBOOK_CLIENT_ID,
+	    clientSecret: process.env.FACEBOOK_SECRET,
+	    callbackURL: process.env.FACEBOOK_URL,
+	    profileFields: ['email','first_name','last_name','gender','profileUrl','picture.type(large)']
+	  },
+	  function(accessToken, refreshToken, profile, done) {
+	    	process.nextTick(function(){
+	    		User.findOne({'facebook.id': profile.id}, function(err, user){
+	    			if(err)
+	    				return done(err);
+	    			if(user)
+	    			{
+	    					return done(null, user);
+	    			}
+	    			else {
 	    			    
-	//     		User.findOne({$or:[{'local.email': profile.emails[0].value},{'google.email': profile.emails[0].value},{'twitter.email': profile.emails[0].value}]}, function(err, userE){
-	//     			    	if(err)
-	//     				return done(err);
-	//     			if(userE)
-	//     			{
-	//     				userE.facebook.id = profile.id;
-	//     				userE.facebook.token = accessToken;
-	//     				 userE.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-	//     				userE.facebook.email = profile.emails[0].value;
+	    		User.findOne({$or:[{'local.email': profile.emails[0].value},{'google.email': profile.emails[0].value},{'twitter.email': profile.emails[0].value}]}, function(err, userE){
+	    			    	if(err)
+	    				return done(err);
+	    			if(userE)
+	    			{
+	    				userE.facebook.id = profile.id;
+	    				userE.facebook.token = accessToken;
+	    				 userE.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+	    				userE.facebook.email = profile.emails[0].value;
 
-	//     				userE.save(function(err){
-	//     					if(err)
-	//     						throw err;
-	//     					return done(null, userE);
-	//     				})
-	//     			}
+	    				userE.save(function(err){
+	    					if(err)
+	    						throw err;
+	    					return done(null, userE);
+	    				})
+	    			}
 	    			
-	//     			else {
+	    			else {
 	    			
-	//     				var newUser = new User();
-	//     				newUser.facebook.id = profile.id;
-	//     				newUser.facebook.token = accessToken;
-	//     				 newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-	//     				newUser.facebook.email = profile.emails[0].value;
-	//     			    newUser.profilePic=profile.photos[0].value
- //                       newUser.local.email = profile.emails[0].value;
- //                       newUser.local.isVerified = true;
- //                       newUser.date=new Date();
-	//     				newUser.save(function(err){
-	//     					if(err)
-	//     						throw err;
-	//     					return done(null, newUser);
-	//     				})
+	    				var newUser = new User();
+	    				newUser.facebook.id = profile.id;
+	    				newUser.facebook.token = accessToken;
+	    				 newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+	    				newUser.facebook.email = profile.emails[0].value;
+	    			    newUser.profilePic=profile.photos[0].value
+                        newUser.local.email = profile.emails[0].value;
+                        newUser.local.isVerified = true;
+                        newUser.date=new Date();
+	    				newUser.save(function(err){
+	    					if(err)
+	    						throw err;
+	    					return done(null, newUser);
+	    				})
 	    			    
-	//     			}
+	    			}
 	    			    
-	//     			})    
+	    			})    
 	    			    
-	//     				console.log(profile);
-	//     			}
-	//     		});
-	//     	});
-	//     }
+	    				console.log(profile);
+	    			}
+	    		});
+	    	});
+	    }
 
-	// ));
+	));
     
     
     
@@ -159,73 +159,73 @@ module.exports = function(passport) {
     
     
     
- //   passport.use(new GoogleStrategy({
-	//     clientID: process.env.GOOGLE_ID,
-	//     clientSecret: process.env.GOOGLE_SECRET,
-	//     callbackURL: process.env.GOOGLE_URL,
-	//     profileFields: ['email','first_name','last_name','gender','profileUrl','picture.type(large)']
-	//   },
-	//   function(accessToken, refreshToken, profile, done) {
-	//     	process.nextTick(function(){
-	//     		User.findOne({'google.id': profile.id}, function(err, user){
-	//     			if(err)
-	//     				return done(err);
-	//     			if(user)
-	//     				return done(null, user);
-	//     			else {
+    passport.use(new GoogleStrategy({
+	    clientID: process.env.GOOGLE_ID,
+	    clientSecret: process.env.GOOGLE_SECRET,
+	    callbackURL: process.env.GOOGLE_URL,
+	    profileFields: ['email','first_name','last_name','gender','profileUrl','picture.type(large)']
+	  },
+	  function(accessToken, refreshToken, profile, done) {
+	    	process.nextTick(function(){
+	    		User.findOne({'google.id': profile.id}, function(err, user){
+	    			if(err)
+	    				return done(err);
+	    			if(user)
+	    				return done(null, user);
+	    			else {
 	    			    
-	//     		User.findOne( {$or:[{'local.email': profile.emails[0].value},{'facebook.email': profile.emails[0].value},{'twitter.email': profile.emails[0].value}]}, function(err, userE){
-	//     			    	if(err)
-	//     				return done(err);
-	//     			if(userE)
-	//     			{
-	//     			    console.log(profile);
-	//     				userE.google.id = profile.id;
-	//     				userE.google.token = accessToken;
-	//     				userE.google.name = profile.displayName;
-	//     				userE.google.email = profile.emails[0].value;
-	//     				userE.save(function(err){
-	//     					if(err)
-	//     						throw err;
-	//     					return done(null, userE);
-	//     				})
-	//     			}
+	    		User.findOne( {$or:[{'local.email': profile.emails[0].value},{'facebook.email': profile.emails[0].value},{'twitter.email': profile.emails[0].value}]}, function(err, userE){
+	    			    	if(err)
+	    				return done(err);
+	    			if(userE)
+	    			{
+	    			    console.log(profile);
+	    				userE.google.id = profile.id;
+	    				userE.google.token = accessToken;
+	    				userE.google.name = profile.displayName;
+	    				userE.google.email = profile.emails[0].value;
+	    				userE.save(function(err){
+	    					if(err)
+	    						throw err;
+	    					return done(null, userE);
+	    				})
+	    			}
 	    			
-	//     			else {
-	//     			console.log(profile);
-	//     			var newUser = new User();
-	//     				console.log(profile)
-	//     				newUser.google.id = profile.id;
-	//     				newUser.google.token = accessToken;
-	//     				newUser.google.name = profile.displayName;
-	//     				newUser.google.email = profile.emails[0].value;
-	//     				 newUser.local.email = profile.emails[0].value;
-	//     				 newUser.local.isVerified = true;
-	//     				 newUser.date=new Date();
-	//     				var newSize="300"
- //                       var str =profile._json.image.url
-	//     				var res = str.split("?sz=50")[0]+"?sz="+newSize
-	//     				newUser.profilePic=res;
+	    			else {
+	    			console.log(profile);
+	    			var newUser = new User();
+	    				console.log(profile)
+	    				newUser.google.id = profile.id;
+	    				newUser.google.token = accessToken;
+	    				newUser.google.name = profile.displayName;
+	    				newUser.google.email = profile.emails[0].value;
+	    				 newUser.local.email = profile.emails[0].value;
+	    				 newUser.local.isVerified = true;
+	    				 newUser.date=new Date();
+	    				var newSize="300"
+                        var str =profile._json.image.url
+	    				var res = str.split("?sz=50")[0]+"?sz="+newSize
+	    				newUser.profilePic=res;
 
-	//     				newUser.save(function(err){
-	//     					if(err)
-	//     						throw err;
-	//     					return done(null, newUser);
-	//     				})
+	    				newUser.save(function(err){
+	    					if(err)
+	    						throw err;
+	    					return done(null, newUser);
+	    				})
 	    			    
-	//     			}
+	    			}
 	    			    
-	//     			})    
+	    			})    
 	    			    
-	//     				console.log(profile);
+	    				console.log(profile);
 	    			    
 	  
-	//     			}
-	//     		});
-	//     	});
-	//     }
+	    			}
+	    		});
+	    	});
+	    }
 
-	// ));
+	));
 	
 	
 	
